d50d0eef65f21d992d7b44fcbb656bff6505664e
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-09-25 17:43:05 -0300

    Adicionado fixture

---
+++ Traits.Tests/ClienteFixtureTests.cs
@@ -0,0 +1,46 @@
+﻿using Traits.Tests.Fixture;
+using Xunit;
+
+namespace Traits.Tests
+{
+    [Collection(nameof(ClienteCollection))]
+    public class ClienteFixtureTests
+    {
+        private readonly ClienteTestsFixture _fixture;
+
+        public ClienteFixtureTests(ClienteTestsFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = "Novo cliente valido")]
+        [Trait("Categoria", "Testes Cliente com fixture")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // arranje
+            var cliente = _fixture.GerarClienteValido();
+
+            //act
+            var result = cliente.EhValido();
+
+            //assert
+            Assert.True(result);
+            Assert.Empty(cliente.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "Novo cliente invalido")]
+        [Trait("Categoria", "Testes Cliente com fixture")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // arranje
+            var cliente = _fixture.GerarClienteInvalido();
+
+            //act
+            var result = cliente.EhValido();
+
+            //assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
---
+++ Traits.Tests/Fixture/ClienteTestsFixture.cs
@@ -0,0 +1,41 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Traits.Tests.Fixture
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture> { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+
+        public Cliente GerarClienteValido()
+        {
+            return new Cliente(
+                    Guid.NewGuid(),
+                    "Willian",
+                    "Menezes",
+                    DateTime.Now.AddYears(-25),
+                    "willian@hotmail.com",
+                    true,
+                    DateTime.Now);
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            return new Cliente(
+                    Guid.NewGuid(),
+                    "",
+                    "Menezes",
+                    DateTime.Now.AddYears(-25),
+                    "willian@hotmail.com",
+                    true,
+                    DateTime.Now);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
