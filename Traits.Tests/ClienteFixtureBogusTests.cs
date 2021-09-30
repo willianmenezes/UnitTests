75a1edc88bfca2c2a1a0d95f9f6230b7b4efde86
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-09-30 13:01:40 -0300

    Adicionado Bogus, para a geracao de dados humanos

---
+++ Traits.Tests/ClienteFixtureBogusTests.cs
@@ -0,0 +1,46 @@
+﻿using Traits.Tests.Fixture;
+using Xunit;
+
+namespace Traits.Tests
+{
+    [Collection(nameof(ClienteCollectionBogus))]
+    public class ClienteFixtureBogusTests
+    {
+        private readonly ClienteTestsFixtureBogus _fixture;
+
+        public ClienteFixtureBogusTests(ClienteTestsFixtureBogus fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = "Novo cliente valido bogus")]
+        [Trait("Categoria", "Testes Cliente com fixture bogus")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // arranje
+            var cliente = _fixture.GerarClienteValido();
+
+            //act
+            var result = cliente.EhValido();
+
+            //assert
+            Assert.True(result);
+            Assert.Empty(cliente.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "Novo cliente invalido bogus")]
+        [Trait("Categoria", "Testes Cliente com fixture bogus")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // arranje
+            var cliente = _fixture.GerarClienteInvalido();
+
+            //act
+            var result = cliente.EhValido();
+
+            //assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
---
+++ Traits.Tests/Fixture/ClienteTestsFixtureBogus.cs
@@ -0,0 +1,50 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Traits.Tests.Fixture
+{
+    [CollectionDefinition(nameof(ClienteCollectionBogus))]
+    public class ClienteCollectionBogus : ICollectionFixture<ClienteTestsFixtureBogus> { }
+
+    public class ClienteTestsFixtureBogus : IDisposable
+    {
+
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            return new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FindName(gender: genero),
+                    f.Name.LastName(gender: genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                    "",
+                    true,
+                    DateTime.Now))
+                .RuleFor(c => c.Email, (f,c) => f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            return new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FindName(gender: genero),
+                    f.Name.LastName(gender: genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                    "",
+                    false,
+                    DateTime.Now));
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
